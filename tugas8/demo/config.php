4f81ce9abfd54e069c18a577a642dacebf50f4cb
YOGA3321 <ageng.prayoga321@gmail.com>
2025-10-22 20:05:41 +0700

    Tugas 8

--- tugas8/demo/config.php
+++ tugas8/demo/config.php
@@ -1,8 +1,13 @@
 <?php
+// define('DB_HOST', 'localhost');
+// define('DB_USER', 'pendaftaran_tugas');
+// define('DB_PASS', '');
+// define('DB_NAME', 'pendaftaran_tugas');
+
 define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'pendaftaran_tugas');
+define('DB_USER', 'u116133173_daftar_tugas');
+define('DB_PASS', '@Yogabd46');
+define('DB_NAME', 'u116133173_daftar_tugas');
 
 $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 if ($conn->connect_error) {
